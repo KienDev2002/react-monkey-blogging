dea098ff968657e58a958805af090366fdec0b47
KienDev <ngotrungkien2k2@gmail.com>
2022-12-29 10:04:22 +0700

    Code giao diện trang SignUp phần 2

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -16,6 +16,44 @@ const SignUpPageStyles = styled.div`
         font-size: 4rem;
         margin-bottom: 60px;
     }
+
+    .field {
+        display: flex;
+        flex-direction: column;
+        row-gap: 20px;
+        align-items: flex-start;
+    }
+
+    .label {
+        color: ${(props) => props.theme.grayDark};
+        font-size: 1.6rem;
+        font-weight: 600;
+        cursor: pointer;
+    }
+
+    .input {
+        width: 100%;
+        padding: 20px;
+        background-color: ${(props) => props.theme.grayLight};
+        border-radius: 8px;
+        font-weight: 500;
+        transition: all 0.2s linear;
+        border: 1px solid transparent;
+    }
+
+    .input:focus {
+        background-color: #fff;
+        border-color: ${(props) => props.theme.primary};
+    }
+
+    .input::placeholder {
+        color: #84878b;
+    }
+
+    .form {
+        max-width: 600px;
+        margin: 0 auto;
+    }
 `;
 
 const SignUpPage = () => {
@@ -28,6 +66,19 @@ const SignUpPage = () => {
                     alt="monkey-blogging"
                 />
                 <h1 className="heading">Monkey Blogging</h1>
+                <form className="form">
+                    <div className="field">
+                        <label htmlFor="fullname" className="label">
+                            Fullname
+                        </label>
+                        <input
+                            type="text"
+                            className="input"
+                            id="fullname"
+                            placeholder="Enter your fullname"
+                        />
+                    </div>
+                </form>
             </div>
         </SignUpPageStyles>
     );
