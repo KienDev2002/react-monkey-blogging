bb01d4db229afdb47793a8ac5d7cf92fa3151f3b
KienDev <ngotrungkien2k2@gmail.com>
2022-12-30 23:41:04 +0700

    Coding all UI home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,8 @@ import { auth } from "~/components/firebase/firebase-config";
 import Header from "~/components/layout/Header";
 import Layout from "~/components/layout/Layout";
 import HomeBanner from "~/module/home/HomeBanner";
+import HomeFeature from "~/module/home/HomeFeature";
+import HomeNewest from "~/module/home/HomeNewest";
 
 const HomePageStyles = styled.div``;
 
@@ -16,6 +18,8 @@ const HomePage = () => {
         <HomePageStyles>
             <Layout>
                 <HomeBanner></HomeBanner>
+                <HomeFeature></HomeFeature>
+                <HomeNewest></HomeNewest>
             </Layout>
         </HomePageStyles>
     );
