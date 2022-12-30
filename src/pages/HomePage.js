2fee8bbbe543f8ed0941688ea3606b49586bb90b
KienDev <ngotrungkien2k2@gmail.com>
2022-12-30 21:44:33 +0700

    Code giao diện block Header phần 1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,19 @@
 import { signOut } from "firebase/auth";
 import React from "react";
+import styled from "styled-components";
 import { auth } from "~/components/firebase/firebase-config";
+import Header from "~/layout/Header";
+
+const HomePageStyles = styled.div``;
 
 const HomePage = () => {
     const handleSignOut = () => {
         signOut(auth);
     };
     return (
-        <div>
-            <button onClick={handleSignOut}>Sign out</button>
-        </div>
+        <HomePageStyles>
+            <Header></Header>
+        </HomePageStyles>
     );
 };
 
