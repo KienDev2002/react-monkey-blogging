1cd7cf4c87ea76c3d5e7cce3bd64ab9a040aa4ee
KienDev <ngotrungkien2k2@gmail.com>
2023-01-01 11:08:10 +0700

    Hiển thị danh sách Danh mục (category)

--- src/module/category/CategoryManage.js
+++ src/module/category/CategoryManage.js
@@ -1,13 +1,82 @@
+import { collection, onSnapshot } from "firebase/firestore";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { ActionDelete, ActionEdit, ActionView } from "~/components/action";
+import { db } from "~/components/firebase/firebase-config";
+import { LabelStatus } from "~/components/label";
+import { Table } from "~/components/table";
 import DashboardHeading from "~/drafts/DashboardHeading";
+import { catogoryStatus } from "~/utils/constants";
 
 const CategoryManage = () => {
+    const [categoryList, setCategoryList] = useState([]);
+    useEffect(() => {
+        const colRef = collection(db, "categories");
+        onSnapshot(colRef, (snapshot) => {
+            const results = [];
+            snapshot.forEach((doc) => {
+                results.push({
+                    id: doc.id,
+                    ...doc.data(),
+                });
+            });
+            setCategoryList(results);
+        });
+    }, []);
+    console.log(categoryList);
     return (
         <div>
             <DashboardHeading
                 title="Categories"
                 desc="Manage your category"
             ></DashboardHeading>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Slug</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {categoryList.length > 0 &&
+                        categoryList.map((category) => (
+                            <tr key={category.id}>
+                                <td>{category.id}</td>
+                                <td>{category.name}</td>
+                                <td>
+                                    <span className="italic text-gray-400">
+                                        {category.slug}
+                                    </span>
+                                </td>
+                                <td>
+                                    {category.status ===
+                                        catogoryStatus.APPROVED && (
+                                        <LabelStatus type="success">
+                                            Approved
+                                        </LabelStatus>
+                                    )}
+                                    {category.status ===
+                                        catogoryStatus.UNAPPROVED && (
+                                        <LabelStatus type="warning">
+                                            Unapproved
+                                        </LabelStatus>
+                                    )}
+                                </td>
+                                <td>
+                                    <div className="flex items-center gap-x-3">
+                                        <ActionView></ActionView>
+                                        <ActionEdit></ActionEdit>
+                                        <ActionDelete></ActionDelete>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                </tbody>
+            </Table>
         </div>
     );
 };
